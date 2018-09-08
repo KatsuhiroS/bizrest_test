8986ca5bf5805b8386a38fdebcaf5dff0efa6b71
Katsuhiro <katsuhiro@MacBookPro.local>
2018-09-08 15:26:22 +0900

    modify HTML5ERROR

--- app/javascript/packs/components/Header/SubjectsButton.jsx
+++ app/javascript/packs/components/Header/SubjectsButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from 'prop-types'
 
 const SubjectsButton = props => (
-  <div>
+  <div onClick={() => location.reload()}>
     <button>
       <a href="/subjects">レッスン一覧</a>
     </button>
