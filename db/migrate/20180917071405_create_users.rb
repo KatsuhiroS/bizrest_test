2916bcee0adb255f1ea4dc981c55e711816fa1a3
Kite0301 <kaito.titech@gmail.com>
2018-09-17 16:21:23 +0900

    Add user model

---
+++ db/migrate/20180917071405_create_users.rb
@@ -0,0 +1,19 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :crypted_password
+      t.string :password_salt
+      t.string :persistence_token
+      # 以下のカラムはAuthlogicが自動で更新してくれる
+      t.integer   :login_count, default: 0, null: false
+      t.integer   :failed_login_count, default: 0, null: false
+      t.datetime  :last_request_at
+      t.datetime  :current_login_at
+      t.datetime  :last_login_at
+      t.string    :current_login_ip
+      t.string    :last_login_ip
+      t.timestamps
+    end
+  end
+end
