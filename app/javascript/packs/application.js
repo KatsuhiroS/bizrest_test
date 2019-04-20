525c36310801407e0ff32778622e5a30479e4e70
TomohiroTsubaki <ttsubaki928@gmail.com>
2019-04-20 20:13:20 +0900

    Change js directory

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,21 +1,13 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
-
 import React from 'react'
 import { render } from 'react-dom'
-import Top from './components/Top/Top'
-import Subjects from './components/Subjects/Subjects'
 import { DragDropContextProvider } from 'react-dnd'
 import HTML5Backend from 'react-dnd-html5-backend'
-import MypageEdit from './components/MypageEdit/MypageEdit'
-import Accounting from './components/Lessons/Accounting'
-import AccountingStudy from './components/Study/AccountingStudy'
+
+import Top from '../src/components/Top/Top'
+import Subjects from '../src/components/Subjects/Subjects'
+import MypageEdit from '../src/components/MypageEdit/MypageEdit'
+import Accounting from '../src/components/Lessons/Accounting'
+import AccountingStudy from '../src/components/Study/AccountingStudy'
 
 document.addEventListener('turbolinks:load', () => {
   const rootComponent = (() => {
