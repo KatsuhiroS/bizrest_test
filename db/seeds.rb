15345fc9e72f57442ad5714a38aa9aa7d69a4a65
TomohiroTsubaki <ttsubaki928@gmail.com>
2019-04-20 21:12:59 +0900

    Create subject model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Subject.create(
+  title: '会計入門',
+  explanation: '世界で一番わかりやすい会計入門の教材です',
+  order: 1
+)
