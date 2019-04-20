24ad38cfb81523914a5c0e385733567765fc450c
TomohiroTsubaki <ttsubaki928@gmail.com>
2019-04-20 22:20:10 +0900

    Add before_action

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,14 +1,8 @@
 class AppsController < ApplicationController
-  before_action :authenticate_user!, only: [:mypage, :subjects, :accounting_study]
+  before_action :authenticate_user!, only: [:accounting_study]
   before_action :forbid_login_user!, only: [:top]
 
   def top
-  end
-
-  def mypage
-  end
-
-  def subjects
   end
 
   def accounting_study
--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,4 +1,6 @@
 class SubjectsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @subjects = Subject.all
   end
