136eb438c673dc34e5e386f9dbc583a9949dbf0d
TomohiroTsubaki <ttsubaki928@gmail.com>
2019-04-26 19:35:02 +0900

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
 Subject.create(
-  title: '会計入門',
-  explanation: '世界で一番わかりやすい会計入門の教材です',
+  title: '会計',
+  explanation: '世界で一番わかりやすい会計の教材です',
+  image: '',
+  slug: 'accounting',
   order: 1
+)
+
+# 上のSubjectのデータがid:1にならないとデータ作られないので注意
+Lesson.create(
+  name: '会計入門',
+  subject_id: 1
 )
