cb4962b45bdbac7a4ca70c0b85479e3e56044ed5
TomohiroTsubaki <ttsubaki928@gmail.com>
2019-06-13 23:04:13 +0900

    Delete unnecessary import

--- app/javascript/packs/routes.js
+++ app/javascript/packs/routes.js
@@ -1,8 +1,4 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { DragDropContextProvider } from 'react-dnd'
-import HTML5Backend from 'react-dnd-html5-backend'
-
 import Top from '../src/components/Top/Top'
 import Subjects from '../src/components/Subjects'
 import Lessons from '../src/components/Lessons'
